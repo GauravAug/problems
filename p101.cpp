5baa276b1921b7e6722a0a7eed89558988dfbedb
GauravAug <gaurav.yadav.spt10@gmail.com>
2023-10-02 20:56:11 +0530

    chapter gross salary

---
+++ p101.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int B,da,hra,GA;
+    cin >> B ;
+    da = B * 0.4;
+    hra = B * 0.2 ;
+    GA = B+hra+da ;
+    cout<< da <<endl;
+    cout<< hra <<endl;
+    cout<< GA <<endl;
+
+    return 0 ;
+}
